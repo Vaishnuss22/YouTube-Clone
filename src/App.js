4a079f88d85b7e3951c954e26f9366974f16148f
Vaishnuss22 <vaishnutejass22@gmail.com>
2023-05-29 19:41:17 +0530

    update check msg

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
     <h1>YouTube</h1>
+    <h1>Commit Check</h1>
     <Navbar />
     </div>
   );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,7 @@ function Navbar(){
     return (
         <div className="navbar">
             <h1>Navbar</h1>
+            <h6>In Navbar Commit</h6>
         </div>
     )
 }
